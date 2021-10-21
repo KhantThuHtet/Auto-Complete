874218cd0a5525945f71344ddf59cb2412aef88c
KhantThuHtet <khantthu7007@gmail.com>
2021-10-21 17:51:47 +0630

    solved

--- js/app.js
+++ js/app.js
@@ -1,58 +1,65 @@
-let inputBox = document.getElementsByClassName('form-control')[0];
-let productContainer = document.getElementsByClassName('productContainer')[0];
+let inputBox = document.getElementById("inputBox");
+let productContainer = document.querySelector(".productContainer");
 
-let fetchData = async ()=>{
-    let respone = await fetch("https://fakestoreapi.com/products");
-    let product = await respone.json();
-    buildUi(product);
-}
+let fetchData = async () => {
+  let response = await fetch("https://fakestoreapi.com/products");
+  let products = await response.json();
+  buildUi(products);
+};
+
 let filteredProduct = [];
 fetchData();
 
-let buildUi = (data) => {
-  let product = data;
 
-  inputBox.addEventListener('keyup', (event) =>{
-    if ( event.key == "ArrowDown" || event.key == "ArrowUp" || event.key == "Enter") {
-        navigateAndShowProduct(event.key);
+
+const buildUi = (products) => {
+  inputBox.addEventListener("keyup", (event) => {
+    if (
+      event.key == "ArrowUp" ||
+      event.key == "ArrowDown" ||
+      event.key == "Enter"
+    ) {
+      navigateAndSelectProduct(event.key);
+      return;
     }
-    productContainer.innerHTML = "";
 
-    if (event.target.value.length == 0) {
-        return;
+    productContainer.innerHTML = "";
+    if (event.target.value == 0) {
+      return;
     }
     let searchText = event.target.value.toLowerCase();
-    filteredProduct = product.filter( element =>{
-        return element.title.toLowerCase().includes(searchText);
+    filteredProduct = products.filter((el) => {
+      let productTitle = el.title.toLowerCase();
+      return productTitle.includes(searchText);
     });
-    filteredProduct.map( element =>{
-        productContainer.innerHTML += `
-        <div class="product" id="${element.id}">
+    filteredProduct.map((element) => {
+      productContainer.innerHTML += `
+      <div class="product" id="${element.id}">
           <div class="product-name">${element.title}</div>
-          <img src="${element.image}" class="product-img" />
-        </div>
-    `;
+          <img src="${element.image}" class="product-img">
+      </div>`;
     });
   });
 };
+
 let indexToSelect = -1;
-const navigateAndShowProduct = (eventKey) =>{
+
+const navigateAndSelectProduct = (key) => {
+  if (key == "ArrowDown") {
     
-    //Error Code...
-    if ( eventKey == "ArrowDown") {
-        indexToSelect += 1;
-        let productIdToShow = document.getElementById(
-          filteredProduct[indexToSelect].id
-        );
-        // console.log(productIdToShow);
-        productIdToShow.style.backgroundColor = "#555048";
-        productIdToShow.classList.add("selected");
-        console.log(productIdToShow);
 
-   } else if ( eventKey == "ArrowUp"){
+    indexToSelect += 1;
+    let productIdToShow = document.getElementById(
+      filteredProduct[indexToSelect].id.toString()
+    );
+    productIdToShow.style.backgroundColor = "#555048";
+    productIdToShow.classList.add("selected");
+    
+
+    console.log(productIdToShow);
+  } else if (key == "ArrowUp") {
     console.log("Arrow Up");
-   } else {
+  } else {
     console.log("Enter");
-   }
-}
-
+  }
+};
